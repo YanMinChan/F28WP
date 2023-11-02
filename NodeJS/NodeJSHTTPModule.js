a3547affe0ac574ff45d256bc21a2722332a9c04
YanMinChan <87971690+YanMinChan@users.noreply.github.com>
2023-11-02 09:46:08 +0000

    adding response.writeHead explanation

--- NodeJS/NodeJSHTTPModule.js
+++ NodeJS/NodeJSHTTPModule.js
@@ -3,6 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function(req, res) {
+    // 200 is the status code ready
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Hello Web World!"); //write a response to the client
     res.end(); //end the response
