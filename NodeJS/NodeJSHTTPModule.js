22b3165cab73d227709d9177868c5a433fe373dc
Yan Min <yc89@hw.ac.uk>
2023-11-06 14:50:49 +0000

    Adding nodejs files

--- NodeJS/NodeJSHTTPModule.js
+++ NodeJS/NodeJSHTTPModule.js
@@ -3,9 +3,12 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function(req, res) {
-    // 200 is the status code ready
+    // HTTP header
+	// 200 is the status code ok
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello Web World!"); //write a response to the client
+    res.write("Hello World! This is my VSCode."); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
+
+  console.log('Server running at http://127.0.0.1:8080/');
