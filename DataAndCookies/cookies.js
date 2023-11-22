42f1650c21e52fa6146dc0581746372075faf551
Yan Min <yc89@hw.ac.uk>
2023-11-22 11:52:13 +0000

    Experimenting around cookies

--- DataAndCookies/cookies.js
+++ DataAndCookies/cookies.js
@@ -14,13 +14,33 @@ function Cookie(name, value, days){ //cookie name, value and how long it will la
 function go(){
 	//add cookie
 	c = Cookie("Skill", "basic", 2) + "; Domain = hw.ac.uk; Path = /";
-	document.cookie = c
+	document.cookie = c;
 	document.getElementById("ck").innerHTML  = "Cookie set: " + c;
 }
 
 function go2(){
 	//delete cookie by giving it date in past
 	c = Cookie("Skill", "anything", -1) + "; Domain = hw.ac.uk; Path = /";
-	document.cookie = c
+	document.cookie = c;
 	document.writeln("Cookie set: " + c);
+}
+
+//smtg wrong with document.cookie as it is empty from bfr but whatever this should work
+function getCookie(){
+	let name = document.getElementById('ckk').value + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let cArray = decodedCookie.split(";");
+	
+	for (let i = 0; i < cArray.length; i++){
+		let cooki = cArray[i];
+		// removing the space
+		while (cooki.charAt(0) == ' ') {
+			cooki = cooki.substring(1);
+		}
+		// loop and find cookie, if found return cookie
+		if (cooki.indexOf(name) == 0) {
+			alert(cooki.substring(name.length, cooki.length));
+		}
+	}
+	alert("idk");
 }
